6dc9839e52d1df4ec9aefd584bf1e7c04e9de377
LazarShockX <donaldbr12@gmail.com>
2024-11-20 11:43:44 +0100

    Auth design and MongDB and Prisma setup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import AuthForm from './components/AuthForm';
+
 export default function Home() {
     return (
         <div className="flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
@@ -9,6 +11,7 @@ export default function Home() {
                     Sign in to your account
                 </h2>
             </div>
+            <AuthForm />
         </div>
     );
 }
